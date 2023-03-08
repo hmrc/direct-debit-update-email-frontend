e556866a44341269ba8ca90de39275203e6d4cfd
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2023-03-08 15:44:51 +0000

    [AC][OPS-10028] service name in header is link (#18)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
     "com.beachape"    %% "enumeratum-play"                               % "1.7.2",
     "org.typelevel"   %% "cats-core"                                     % "2.9.0",
     "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "0.5.0",
-    "uk.gov.hmrc"     %% "payments-email-verification-cor"               % "0.1.0",
+    "uk.gov.hmrc"     %% "payments-email-verification-cor"               % "0.2.0",
     "uk.gov.hmrc"     %% "emailaddress"                                  % "3.7.0"
   // format: ON
   )
