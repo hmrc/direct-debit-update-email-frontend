bb35ee03880bb7f4d09918df63fff20f95cf339e
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2023-04-03 16:31:43 +0100

    [AC] library upgrades (#22)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.14.0"
+  private val bootstrapVersion = "7.15.0"
 
   val compile = Seq(
     // format: OFF
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-28"                    % bootstrapVersion,
-    "uk.gov.hmrc"     %% "play-frontend-hmrc"                            % "6.8.0-play-28",
-    "uk.gov.hmrc"     %% "play-conditional-form-mapping"                 % "1.12.0-play-28",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc"                            % "7.3.0-play-28",
+    "uk.gov.hmrc"     %% "play-conditional-form-mapping"                 % "1.13.0-play-28",
     "com.beachape"    %% "enumeratum-play"                               % "1.7.2",
     "org.typelevel"   %% "cats-core"                                     % "2.9.0",
-    "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "0.6.0",
-    "uk.gov.hmrc"     %% "payments-email-verification-cor"               % "0.2.0",
-    "uk.gov.hmrc"     %% "emailaddress"                                  % "3.7.0"
+    "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "0.7.0",
+    "uk.gov.hmrc"     %% "payments-email-verification-cor"               % "0.3.0",
+    "uk.gov.hmrc"     %% "emailaddress"                                  % "3.8.0"
   // format: ON
   )
 
