86cf7651a4de416663623215ceb569806176f0ff
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2023-12-14 15:58:10 +0000

    [AC][OPS-11454] play 3 upgrade

--- project/WartRemoverSettings.scala
+++ project/WartRemoverSettings.scala
@@ -13,6 +13,7 @@ object WartRemoverSettings {
         Wart.ImplicitConversion,
         Wart.ImplicitParameter,
         Wart.Nothing,
+        Wart.Null,
         Wart.Overloading,
         Wart.Throw,
         Wart.ToString
@@ -23,7 +24,8 @@ object WartRemoverSettings {
         Wart.GlobalExecutionContext,
         Wart.Null,
         Wart.NonUnitStatements,
-        Wart.PublicInference
+        Wart.PublicInference,
+        Wart.SeqApply
       ),
       wartremoverExcluded ++= (baseDirectory.value ** "*.sc").get ++
         (Compile / routes).value
