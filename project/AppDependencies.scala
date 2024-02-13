8e0d1307641b8d0f355a438fcefd5237cb3130b0
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2024-02-13 16:55:22 +0000

    [AC][OPS-11769] library updates (#34)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,17 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.3.0"
+  private val bootstrapVersion = "8.4.0"
 
   val compile = Seq(
     // format: OFF
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-30"                    % bootstrapVersion,
-    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "8.2.0",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "8.5.0",
     "uk.gov.hmrc"     %% "play-conditional-form-mapping-play-30"         % "2.0.0",
     "com.beachape"    %% "enumeratum-play"                               % "1.8.0",
     "org.typelevel"   %% "cats-core"                                     % "2.10.0",
-    "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "0.15.0",
-    "uk.gov.hmrc"     %% "payments-email-verification-cor-play-30"       % "2.0.0",
+    "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "0.16.0",
+    "uk.gov.hmrc"     %% "payments-email-verification-cor-play-30"       % "2.3.0",
     "uk.gov.hmrc"     %% "emailaddress-play-30"                          % "4.0.0"
   // format: ON
   )
@@ -20,7 +20,7 @@ object AppDependencies {
   val test = Seq(
     // format: OFF
     "uk.gov.hmrc"     %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.jsoup"       %  "jsoup"                  % "1.17.1",
+    "org.jsoup"       %  "jsoup"                  % "1.17.2",
   // format: ON
   ).map(_ % Test)
 }
