ac289b9bc060dad392036c4d113dc8cfd1e29da7
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2024-04-22 15:30:47 +0100

    [AC][OPS-12034] library updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,12 +7,12 @@ object AppDependencies {
   val compile = Seq(
     // format: OFF
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-30"                    % bootstrapVersion,
-    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "9.0.0",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "9.8.0",
     "uk.gov.hmrc"     %% "play-conditional-form-mapping-play-30"         % "2.0.0",
     "com.beachape"    %% "enumeratum-play"                               % "1.8.0",
     "org.typelevel"   %% "cats-core"                                     % "2.10.0",
     "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "0.16.0",
-    "uk.gov.hmrc"     %% "payments-email-verification-cor-play-30"       % "2.3.0"
+    "uk.gov.hmrc"     %% "payments-email-verification-cor-play-30"       % "2.4.0"
   // format: ON
   )
 
