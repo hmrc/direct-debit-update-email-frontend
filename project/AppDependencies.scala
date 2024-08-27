be5ba06c254df08246eac5cd8ee3e506f23b66cf
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2024-08-27 13:43:59 +0100

    [JG] OPS-12662 - dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.0.0"
+  private val bootstrapVersion = "9.3.0"
 
   val compile = Seq(
     // format: OFF
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-30"                    % bootstrapVersion,
-    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "10.1.0",
-    "uk.gov.hmrc"     %% "play-conditional-form-mapping-play-30"         % "2.0.0",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "10.10.0",
+    "uk.gov.hmrc"     %% "play-conditional-form-mapping-play-30"         % "3.2.0",
     "com.beachape"    %% "enumeratum-play"                               % "1.8.0",
     "org.typelevel"   %% "cats-core"                                     % "2.12.0",
     "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "0.18.0",
@@ -19,7 +19,7 @@ object AppDependencies {
   val test = Seq(
     // format: OFF
     "uk.gov.hmrc"     %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.jsoup"       %  "jsoup"                  % "1.17.2"
+    "org.jsoup"       %  "jsoup"                  % "1.18.1"
   // format: ON
   ).map(_ % Test)
 }
