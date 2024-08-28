f8166d9ca75d8d5753ef835aa9280e51f389e728
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2024-08-28 13:36:36 +0100

    [JG] OPS-12662 - dependency updates (#40)
    * [JG] OPS-12662 - dependency updates
    
    * [JG] OPS-12662 - updating sbt plugin
    
    * [JG] OPS-12662 - updated cor journeys and bootstrap
    
    * [JG] OPS-12662 - updated backend dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,24 +2,24 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.0.0"
+  private val bootstrapVersion = "9.4.0"
 
   val compile = Seq(
     // format: OFF
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-30"                    % bootstrapVersion,
-    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "10.1.0",
-    "uk.gov.hmrc"     %% "play-conditional-form-mapping-play-30"         % "2.0.0",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "10.10.0",
+    "uk.gov.hmrc"     %% "play-conditional-form-mapping-play-30"         % "3.2.0",
     "com.beachape"    %% "enumeratum-play"                               % "1.8.0",
     "org.typelevel"   %% "cats-core"                                     % "2.12.0",
-    "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "0.18.0",
-    "uk.gov.hmrc"     %% "payments-email-verification-cor-play-30"       % "3.0.0"
+    "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "0.20.0",
+    "uk.gov.hmrc"     %% "payments-email-verification-cor-play-30"       % "3.1.0"
   // format: ON
   )
 
   val test = Seq(
     // format: OFF
     "uk.gov.hmrc"     %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.jsoup"       %  "jsoup"                  % "1.17.2"
+    "org.jsoup"       %  "jsoup"                  % "1.18.1"
   // format: ON
   ).map(_ % Test)
 }
