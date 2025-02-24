1a528894af3feb9854313ab59f12f17ed8a7ca07
Kostas Ntanos <kostas.ntanos@digital.hmrc.gov.uk>
2025-02-24 15:17:28 +0000

    [KN][OPS-12336] Scala 3 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc"     %% "play-conditional-form-mapping-play-30"         % "3.2.0",
     "com.beachape"    %% "enumeratum-play"                               % "1.8.0",
     "org.typelevel"   %% "cats-core"                                     % "2.13.0",
-    "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "1.0.0-SNAPSHOT",
+    "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "1.0.0",
     "uk.gov.hmrc"     %% "payments-email-verification-cor-play-30"       % "4.2.0"
   // format: ON
   )
