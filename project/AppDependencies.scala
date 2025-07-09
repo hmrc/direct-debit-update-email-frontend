556233a50bb53d911e15aa5b7715457f9a9ace32
Kntanos <kostas@ntanos.co.uk>
2025-07-09 11:52:27 +0100

    [KN][OPS-13832] Gov Uk Rebrand

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
+  private val bootstrapVersion = "9.15.0"
 
   val compile = Seq(
     // format: OFF
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-30"                    % bootstrapVersion,
-    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "12.6.0",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "12.7.0",
     "uk.gov.hmrc"     %% "play-conditional-form-mapping-play-30"         % "3.3.0",
     "com.beachape"    %% "enumeratum-play"                               % "1.8.2",
     "org.typelevel"   %% "cats-core"                                     % "2.13.0",
@@ -19,7 +19,7 @@ object AppDependencies {
   val test = Seq(
     // format: OFF
     "uk.gov.hmrc"     %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.jsoup"       %  "jsoup"                  % "1.20.1"
+    "org.jsoup"       %  "jsoup"                  % "1.21.1"
   // format: ON
   ).map(_ % Test)
 }
