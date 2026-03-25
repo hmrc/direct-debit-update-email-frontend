28d2b8ca2e3273c41b6d24c7edac988ae82ac34b
JamieNorth <75728919+JamieNorth@users.noreply.github.com>
2026-03-25 15:47:43 +0000

    [JN][OPS-14747] - Dependency Updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
 
   val compile = Seq(
     // format: OFF
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-30"                    % bootstrapVersion,
-    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "12.28.0",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc-play-30"                    % "13.1.0",
     "uk.gov.hmrc"     %% "play-conditional-form-mapping-play-30"         % "3.4.0",
-    "com.beachape"    %% "enumeratum-play"                               % "1.9.2",
+    "com.beachape"    %% "enumeratum-play"                               % "1.9.6",
     "org.typelevel"   %% "cats-core"                                     % "2.13.0",
     "uk.gov.hmrc"     %% "direct-debit-update-email-backend-cor-journey" % "1.5.0",
     "uk.gov.hmrc"     %% "payments-email-verification-cor-play-30"       % "4.7.0"
